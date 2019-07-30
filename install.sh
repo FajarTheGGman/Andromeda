0a4b8806f7b53bc108edbcd88ff7695899d78a8c
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-07-30 15:36:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 # installing package
 
 echo "[!] Installing Package"
-sleep(1)
+sleep 1
 apt-get update && apt-get upgrade -y
 apt-get install python -y
 pip install urllib3 tkinter
 echo "Done :) "
-sleep(0.5)
+sleep 0.5
 echo "[+] Running Program..."
-sleep(1)
+sleep 1
 python run.py
